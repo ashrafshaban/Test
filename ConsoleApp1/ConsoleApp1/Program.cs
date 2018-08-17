d1815e258eb76ec7bc017fe09e70499d161cd497
ashrafshaban <ashraf2005_fci@yahoo.com>
2018-08-17 13:59:12 +0200

    test

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello amr");
+            Console.WriteLine("hello amr xq");
             Console.ReadLine();
         }
     }
