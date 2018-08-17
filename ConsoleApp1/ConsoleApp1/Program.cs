2596119011fd83b5cc0fc3c4993d39324fafc2e7
Amr Youssef <Amr Youssef@DESKTOP-BIL4R48>
2018-08-17 20:26:54 +0200

    amr publish

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("hello amr xq");
+            Console.WriteLine("hello ashraf");
             Console.ReadLine();
         }
     }
